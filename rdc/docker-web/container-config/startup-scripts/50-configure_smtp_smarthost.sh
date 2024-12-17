c63ef39a8734150edb6af9bd817dc02fd0f27088
Lam Nguyen <lam@calif.io>
2024-12-17 17:18:53 +0700

    feat: send email directly instead of going through mailpit

--- rdc/docker-web/container-config/startup-scripts/50-configure_smtp_smarthost.sh
+++ rdc/docker-web/container-config/startup-scripts/50-configure_smtp_smarthost.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
-
-# SET THE ENV VARIABLE TO MODIFY THE SSMTP MAIL RELAY
-if [ ! -z "${SMTP_SMARTHOST}" ]; then
-  # Replace the line in the msmtp config
-  sed -i "s/^host\s.*/host ${SMTP_SMARTHOST}/g" /etc/msmtprc
-  echo "MSMTP host set to ${SMTP_SMARTHOST}"
-fi
-
-if [ ! -z "${SMTP_PORT}" ]; then
-  # Replace the line in the msmtp config
-  sed -i "s/^port\s.*/port ${SMTP_PORT}/g" /etc/msmtprc
-  echo "MSMTP host set to ${SMTP_PORT}"
-fi
-
-
+sed -i "s/^host\s.*/host ${SMTP_HOST}/g" /etc/msmtprc
+sed -i "s/^host\s.*/host ${SMTP_PORT}/g" /etc/msmtprc
+sed -i "s/^host\s.*/host ${SMTP_USER}/g" /etc/msmtprc
+sed -i "s/^host\s.*/host ${SMTP_PASS}/g" /etc/msmtprc
