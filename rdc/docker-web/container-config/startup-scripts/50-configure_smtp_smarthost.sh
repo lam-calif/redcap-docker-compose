880980d8062483b4773d718d5b1c9a91bf932b68
Lam Nguyen <lam@calif.io>
2024-12-17 17:38:45 +0700

    fix: msmtp config

--- rdc/docker-web/container-config/startup-scripts/50-configure_smtp_smarthost.sh
+++ rdc/docker-web/container-config/startup-scripts/50-configure_smtp_smarthost.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sed -i "s/^host\s.*/host ${SMTP_HOST}/g" /etc/msmtprc
-sed -i "s/^host\s.*/host ${SMTP_PORT}/g" /etc/msmtprc
-sed -i "s/^host\s.*/host ${SMTP_USER}/g" /etc/msmtprc
-sed -i "s/^host\s.*/host ${SMTP_PASS}/g" /etc/msmtprc
+sed -i "s/^port\s.*/port ${SMTP_PORT}/g" /etc/msmtprc
+sed -i "s/^from\s.*/from ${SMTP_USER}/g" /etc/msmtprc
+sed -i "s/^user\s.*/user ${SMTP_USER}/g" /etc/msmtprc
+sed -i "s/^password\s.*/password ${SMTP_PASS}/g" /etc/msmtprc
